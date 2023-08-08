67d1b40b7a8420be91bdc5c884697d0de509cebe
LayneHansen <layne.d.hansen@gmail.com>
2023-08-07 20:53:46 -0400

    added useritem component to pull data into ui

--- github-finder/src/components/layout/Spinner.jsx
+++ github-finder/src/components/layout/Spinner.jsx
@@ -1,35 +1,13 @@
-function Spinner() {spinner.gif
-Footer.jsx
-Navbar.jsx
-Spinner.jsx
-users
-UserResults.jsx
-pages
-App.js
-index.css
-index.js
-.gitignore
-README.md
-craco.config.js
-package-lock.json
-package.json
-tailwind.config.js
-Documentation • Share feedback
-Breadcrumbsgithub-finder-app/src/components/layout/assets
-/spinner.gif
-Latest commit
-bradtraversy
-bradtraversy
-Fetch users and UserList component
-68d1089
- · 
-2 years ago
-History
-File metadata and controls
-2.21 MB
-spinner.gif
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
   return (
-    <div>Spinner</div>
+    <div className='w-100 mt-20'>
+      <img width={180} 
+      className='text-center mx-auto'
+      src={spinner} 
+      alt="Loading..." />
+    </div>
   )
 }
 
