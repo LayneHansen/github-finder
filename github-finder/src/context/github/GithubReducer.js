322d624e7560b4541e45ed672978194c9f35d983
LayneHansen <layne.d.hansen@gmail.com>
2023-09-13 20:51:56 -0400

    cleaned up user results, removed useEffect

--- github-finder/src/context/github/GithubReducer.js
+++ github-finder/src/context/github/GithubReducer.js
@@ -4,8 +4,13 @@ const githubReducer = (state, action) => {
       return {
         ...state,
         users: action.payload,
-        loading: false
+        loading: false,
       }
+      case 'SET_LOADING':
+        return {
+          ...state,
+          loading: true
+        }
     default: return state
   }
 }
